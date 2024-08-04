4b1354bf08ef30898eb24ddf74ce78e18c754a9e
muhammad-halimov <muhammadi.halimov@icloud.com>
2024-08-04 16:50:17 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# WPF To-Do Application
+
+Simple Symfony CRUD app with API and Admin panel.
+- App is Dockerized
+
+## Features
+
+- POST/GET/PATCH/DELETE
+
+## Installation
+
+1. Clone the repository:
+   ```
+   https://github.com/muhammad-halimov/symfony-starter.git
+   ```
+2. Navigate to the project directory:
+   ```
+   cd symfony-starter
+   ```
+3. Run the application:
+   ```
+   symfony server:start
+   ```
+
+## Testing
+
+To run the unit tests for the application, use the built-in unit testing.
+
+The tests cover the basic functionality.
+
+## Contributing
+
+If you find any issues or have suggestions for improvement, feel free to open a new issue or submit a pull request.
+
+## License
+
+Licensed under:
+
+* MIT license (https://opensource.org/license/mit)
+
+## Targeted OS
+
+- Windows NT 10/11
+- GNU/Linux - Distributions
+- BSD/Mach - macOS
---
+++ src/Entity/Products.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ProductsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductsRepository::class)]
+#[ApiResource]
+//#[ApiResource(paginationItemsPerPage: 1)]
+class Products
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $count = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCount(): ?int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): static
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
